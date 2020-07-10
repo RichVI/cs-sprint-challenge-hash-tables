c7e4d75acf32f87ebae3e2567e047941a9c560ed
RichVI <richardwang2050@gmail.com>
2020-07-10 10:35:58 -0700

    ex4_complete

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,33 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    result = []
 
+    # Storage all intergers in the cache
+    for i in a:
+        # print("I", i)
+        if i not in cache:
+            cache[i] = i
+
+    # Look for the counter part of the integer
+    for j in cache:
+        if cache[j] < 0 and -cache[j] in cache:
+            print(-cache[j])
+            result.append(abs(cache[j]))
     return result
+
+# cache {
+#     -1: -1,
+#     -2: -2,
+#      1: 1,
+#      2: 2,
+#      3: 3,
+#      4: 4,
+#      -4: -4 
+# }
+
+
 
 
 if __name__ == "__main__":
